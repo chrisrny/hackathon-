35f97ae9dea98970000d2b66a7423be83cc6a19b
Arthur <arthur.barvidat@epitech.eu>
2026-01-15 13:08:31 +0100

    test

--- my-app/lib/mongodb.js
+++ my-app/lib/mongodb.js
@@ -2,19 +2,8 @@ import { MongoClient } from "mongodb";
 
 const uri = "mongodb+srv://Arthur:Charlotte@database2.qccpigy.mongodb.net/DataBase2?retryWrites=true&w=majority";
 
-let client;
-let clientPromise;
-
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
+let client = new MongoClient(uri);
+let clientPromise = client.connect();
 
 export default clientPromise;
 
